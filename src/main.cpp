9b3b4faec84248377049ae18b344821283576047
HugoHolm <hugostalhandskeholm@gmail.com>
2025-10-24 15:49:36 +0200

    changes to main.cpp to improve seeding

--- src/main.cpp
+++ src/main.cpp
@@ -4,13 +4,11 @@
 #include <fstream>
 #include <sstream>
 #include <string>
-#include <cstdlib>
-#include <ctime>
 #include <stdexcept>
 
 int main(int argc, char* argv[]) {
-    if (argc < 5) {
-        std::cerr << "Usage: " << argv[0] << " <k> <input.txt> <length> <seed_kgram>" << std::endl;
+    if (argc < 4) {
+        std::cerr << "Usage: " << argv[0] << " <k> <input.txt> <length> [seed_kgram]" << std::endl;
         return 1;
     }
     // Defining variables for later 
@@ -21,23 +19,32 @@ int main(int argc, char* argv[]) {
     int text_length;
 
     // Assigning value to variables from input 
-    seed = argv[4];
+    
     text_length = std::stoi(argv[3]);
     k = std::stoi(argv[1]);
-
-    if (seed.size() != static_cast<std::size_t>(k)) {
-        std::cerr << "<kgram_seed> must have same size as <k>" << std::endl; 
-        return 1;
-    }
-
     file.open(argv[2]);
     if (!file) {
-        std::cerr << "Error: could not open input.txt" << std::endl;
+        std::cerr << "Error: could not open textfile." << std::endl;
     return 1; 
     }
     std::stringstream buffer;
     buffer << file.rdbuf();
     text = buffer.str();
+
+    if (argc == 5) {
+        seed = argv[4];
+        if (seed.size() != static_cast<std::size_t>(k)) {
+            std::cerr << "<kgram_seed> must have same size as <k>" << std::endl; 
+            return 1;
+        }
+    }
+    else {
+        if (text.size() < static_cast<size_t>(k)) {
+            std::cerr << "Error: training text is shorter than k = " << k << std::endl;
+            return 1;
+        }
+        seed = text.substr(0,k);
+    }
 
     // Building model
     KGram model(k);
